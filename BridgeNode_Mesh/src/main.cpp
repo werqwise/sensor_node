870e652d1ba76d4fe8a88a4f7e0765bcc925083c
Nauman3S <naumanshakir3s@gmail.com>
2024-04-13 23:05:27 +0200

    added BridgeNode_Mesh

---
+++ BridgeNode_Mesh/src/main.cpp
@@ -0,0 +1,213 @@
+// This is a Bridge node send and receive data to/from Raspberry Pi over USB Serial connection.
+
+#include <Arduino.h>
+#include "SStack.h"
+#define LED 2 // GPIO number of connected LED, ON ESP-12 IS GPIO2
+
+#define BLINK_PERIOD 3000  // milliseconds until cycle repeat
+#define BLINK_DURATION 100 // milliseconds LED is on for
+
+#include "configs.h"
+// Prototypes
+void sendMessage();
+void receivedCallback(uint32_t from, String &msg);
+void newConnectionCallback(uint32_t nodeId);
+void changedConnectionCallback();
+void nodeTimeAdjustedCallback(int32_t offset);
+void delayReceivedCallback(uint32_t from, int32_t delay);
+
+Scheduler userScheduler; // to control your personal task
+painlessMesh mesh;
+
+
+bool calc_delay = false;
+SimpleList<uint32_t> nodes;
+
+void sendMessage();                                                  // Prototype
+Task taskSendMessage(TASK_SECOND * 0.5, TASK_FOREVER, &sendMessage); // start with a one second interval
+
+// Task to blink the number of nodes
+Task blinkNoNodes;
+bool onFlag = false;
+String TrackerID = "";
+
+class BridgeManager
+{
+public:
+  static BridgeManager *instance;
+  std::map<String, std::pair<String, int>> nodes; // MAC, (SensorData, RSSI)
+  BridgeManager(painlessMesh &meshRef) : mesh(meshRef)
+  {
+
+    mesh.setDebugMsgTypes(ERROR | DEBUG); // set before init() so that you can see error messages
+
+    mesh.init(MESH_SSID, MESH_PASSWORD, &userScheduler, MESH_PORT);
+    mesh.onReceive(receivedCallback);
+    mesh.onNewConnection(newConnectionCallback);
+    mesh.onChangedConnections(changedConnectionCallback);
+    mesh.onNodeTimeAdjusted(nodeTimeAdjustedCallback);
+    mesh.onNodeDelayReceived(delayReceivedCallback);
+    mesh.setContainsRoot(true);
+  }
+
+  void sendBroadcast(const String &message)
+  {
+    JsonDocument doc;
+    doc["command"] = "broadcast";
+    doc["data"] = message;
+    String jsonMessage;
+    serializeJson(doc, jsonMessage);
+    mesh.sendBroadcast(jsonMessage);
+  }
+
+  void sendDataToUART()
+  {
+    JsonDocument doc;
+    doc["nodeCount"] = nodes.size();
+    for (auto &node : nodes)
+    {
+      JsonArray nodeData = doc.createNestedArray(node.first);
+      nodeData.add(node.second.first);  // Sensor data
+      nodeData.add(node.second.second); // RSSI
+    }
+    String jsonMessage;
+    serializeJson(doc, jsonMessage);
+    Serial.println(jsonMessage);
+  }
+
+  int getNodeCount() const
+  {
+    return nodes.size();
+  }
+
+  static void receivedCallback(uint32_t from, String &msg)
+  {
+    JsonDocument doc;
+    deserializeJson(doc, msg);
+    String mac = doc["macAddress"];
+    String data = doc["data"];
+    int rssi = doc["rssi"];
+    instance->nodes[mac] = {data, rssi};
+  }
+
+private:
+  painlessMesh &mesh;
+};
+
+BridgeManager *BridgeManager::instance = nullptr;
+
+void setup()
+{
+  Serial.begin(115200);
+  TrackerID = String(WiFi.macAddress());
+
+  Serial.print("TrackerID: ");
+  Serial.println(TrackerID);
+  pinMode(LED, OUTPUT);
+
+  userScheduler.addTask(taskSendMessage);
+  taskSendMessage.enable();
+
+  blinkNoNodes.set(BLINK_PERIOD, (mesh.getNodeList().size() + 1) * 2, []()
+                   {
+                     // If on, switch off, else switch on
+                     if (onFlag)
+                       onFlag = false;
+                     else
+                       onFlag = true;
+                     blinkNoNodes.delay(BLINK_DURATION);
+
+                     if (blinkNoNodes.isLastIteration())
+                     {
+                       // Finished blinking. Reset task for next run
+                       // blink number of nodes (including this node) times
+                       blinkNoNodes.setIterations((mesh.getNodeList().size() + 1) * 2);
+                       // Calculate delay based on current mesh time and BLINK_PERIOD
+                       // This results in blinks between nodes being synced
+                       blinkNoNodes.enableDelayed(BLINK_PERIOD -
+                                                  (mesh.getNodeTime() % (BLINK_PERIOD * 1000)) / 1000);
+                     } });
+  userScheduler.addTask(blinkNoNodes);
+  blinkNoNodes.enable();
+
+  randomSeed(analogRead(A0));
+  BridgeManager::instance = new BridgeManager(mesh);
+}
+
+void loop()
+{
+  mesh.update();
+  digitalWrite(LED, !onFlag);
+}
+
+void sendMessage()
+{
+
+  if (Serial.available())
+  {
+    String data = Serial.readStringUntil('\n');
+    BridgeManager::instance->sendBroadcast(data);
+  }
+  taskSendMessage.setInterval(TASK_SECOND * 0.5); // Adjust as needed
+}
+
+void receivedCallback(uint32_t from, String &msg)
+{
+
+  JsonDocument doc;
+  deserializeJson(doc, msg);
+  String mac = doc["macAddress"];
+  String data = doc["data"];
+  int rssi = doc["rssi"];
+  BridgeManager::instance->nodes[mac] = {data, rssi};
+}
+
+void newConnectionCallback(uint32_t nodeId)
+{
+  // Reset blink task
+  onFlag = false;
+  blinkNoNodes.setIterations((mesh.getNodeList().size() + 1) * 2);
+  blinkNoNodes.enableDelayed(BLINK_PERIOD - (mesh.getNodeTime() % (BLINK_PERIOD * 1000)) / 1000);
+
+  // Serial.printf("--> startHere: New Connection, nodeId = %u\n", nodeId);
+  // Serial.printf("--> startHere: New Connection, %s\n", mesh.subConnectionJson(true).c_str());
+}
+
+void changedConnectionCallback()
+{
+  Serial.printf("Changed connections\n");
+  // Reset blink task
+  onFlag = false;
+  blinkNoNodes.setIterations((mesh.getNodeList().size() + 1) * 2);
+  blinkNoNodes.enableDelayed(BLINK_PERIOD - (mesh.getNodeTime() % (BLINK_PERIOD * 1000)) / 1000);
+
+  nodes = mesh.getNodeList();
+  // Serial.printf("nodeslist:::");
+  // Serial.println(mesh.subConnectionJson());
+  // Serial.println("nodes handler id list");
+
+  // Serial.printf("Num nodes: %d\n", nodes.size());
+  // Serial.printf("Node pointer: %d\n", nH.getNodesPointer());
+  // Serial.printf("Connection list:");
+  String NodeRemoved = "";
+  SimpleList<uint32_t>::iterator node = nodes.begin();
+  // nH.printAll();
+  while (node != nodes.end())
+  {
+    Serial.printf(" %u", *node);
+
+    node++;
+  }
+  // Serial.println();
+  calc_delay = true;
+}
+
+void nodeTimeAdjustedCallback(int32_t offset)
+{
+  // Serial.printf("Adjusted time %u. Offset = %d\n", mesh.getNodeTime(), offset);
+}
+
+void delayReceivedCallback(uint32_t from, int32_t delay)
+{
+  // Serial.printf("Delay to node %u is %d us\n", from, delay);
+}
