e75a1681d8731f46ebf52b9a3a4c0a163078d58b
Nauman3S <naumanshakir3s@gmail.com>
2024-04-14 19:27:13 +0200

    fixed ldr handler pin

--- SensorNode_Mesh/lib/ldr_handler/src/ldr_handler.h
+++ SensorNode_Mesh/lib/ldr_handler/src/ldr_handler.h
@@ -1,6 +1,6 @@
-#define LIGHT_SENSOR_PIN 36 // ESP32 pin GIOP36 (ADC0)
+#define LIGHT_SENSOR_PIN 34 // ESP32 pin GIOP36 (ADC0)
 
-int get_ldr()
+int get_ldr() //less value; brighter light
 {
     // reads the input on analog pin (value between 0 and 4095)
     int analogValue = analogRead(LIGHT_SENSOR_PIN);
