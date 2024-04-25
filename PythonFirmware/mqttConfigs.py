c95427907672861d9d7b8331c0b0c3fc3e3de4a0
Nauman3S <naumanshakir3s@gmail.com>
2024-04-25 14:19:28 +0200

    updated sensor node

--- PythonFirmware/mqttConfigs.py
+++
@@ -1,8 +0,0 @@
-# put your aws mqtt broker details below
-host = 'eu-west-2.random.com'
-rootCAPath = 'certs/'
-certificatePath = 'certs/'
-privateKeyPath = 'ceerts/'
-port = '443'
-useWebsocket = False
-clientId = ''
